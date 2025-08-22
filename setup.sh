820727efbdebc064dad637eed2e46dca416dbbb6
Alessandro Da Silva <73776698+Daxer97@users.noreply.github.com>
2025-08-22 18:55:16 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -85,7 +85,8 @@ qm set ${VMID} \
   --ide2 "${STORAGE}:cloudinit" \
   --ipconfig0 ip=dhcp \
   --sshkeys "${SSH_PUBLIC_KEY}" \
-  --efidisk0 "${STORAGE}:vm-${VMID}-efidisk0,efitype=4m,pre-enrolled-keys=1" 
+  --efidisk0 "${STORAGE}:vm-${VMID}-disk-0,efitype=4m,pre-enrolled-keys=1" 
+
 qm resize ${VMID} scsi0 ${DISK_SIZE}
 
 # --- 3. Start VM and Wait for Provisioning (Cloud-Init) ---
